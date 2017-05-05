c075176912fd5b40b578080179c89b1625073eb5
electrone901 <Carbajalluis@rocketmail.com>
2017-05-05 00:58:03 -0400

    add to heroku

--- server.js
+++ server.js
@@ -28,9 +28,8 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json())
 app.use(express.static('./public')) // absolute path /
 // ========================================
-
- app.listen(8000, ()=>console.log('YAY...!! Listening to port 8000'))
-
+const port = process.env.PORT || '5000';
+app.listen(port, ()=>console.log('YAY...!! Listening to port 5000 ' + port))
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, '/views/index.html'))
